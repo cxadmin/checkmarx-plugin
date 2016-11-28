8e17a02b92f741a34d706d9ca249edac4d294a5d
ronah <rona.Hirsch@checkmarx.com>
2016-11-28 10:18:29 +0200

    Add Jenkins Trigger

---
+++ JenkinsTrigger/JenkinsTrigger.ps1
@@ -0,0 +1,6 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[string]$Job
+)
+
+Invoke-WebRequest -Uri "http://cx-jenkins:8080/jenkins/job/$Job/buildWithParameters?token=PluginsToken&PARAMETER=true" -Method POST
